246766d055158dbebf515df7b9bc97a26079330d
lucas-mior <lucas.mior@tutamail.com>
2023-02-09 20:21:39 -0300

    use libc to signal

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-// use nix::unistd::getppid;
 use std::process::exit;
 use std::env;
 use std::fs;
+use libc;
+// use nix::sys::signal;
 
 static NUMBERS: &'static [i32] = &[0, 200, 508, 711, 996, 1394, 1952, 2733, 3827, 5347, 7142];
 static BRIGHT_FILE: &'static str = "brightness";
 static DEF_OPACITY: usize = 8;
+static EXE_NAME: &str = "dwmblocks";
 
 static USAGE: &str = 
 "bright [-+=h]
@@ -46,11 +48,55 @@ fn main() {
     fs::write(BRIGHT_FILE, NUMBERS[index].to_string()).expect("Unable to write file");
     println!("🔆 {}", index);
 
-    let bright_env = env::var("BRIGHT").unwrap();
-    println!("BRIGHT: {}", bright_env);
+    let bright = env::var("BRIGHT").unwrap();
+    println!("BRIGHT: {}", bright);
+    let bright: i32 = bright.parse().unwrap();
     
-    let dirs = fs::read_dir("/proc/");
-    println!("dirs: {:?}", dirs);
+    let dirs = match fs::read_dir("/proc/") {
+        Ok(dirs) => dirs,
+        Err(error) => {
+            println!("Error: {}", error); 
+            exit(1);
+        },
+    };
+
+    for dir in dirs {
+        let d = dir.unwrap();
+        let pid = check_pid(&d.path().to_string_lossy().to_string());
+        if pid != 0 {
+            unsafe {
+                println!("unsafe: {} {}", pid, bright);
+                libc::kill(pid, libc::SIGRTMIN() + bright);
+            }
+            // signal::kill(pid, signal::try_from(bright));
+        }
+    }
 
     return;
+}
+
+fn check_pid(path: &String) -> i32 {
+    let statfile = format!("{}/{}", path, "stat");
+    let data = match fs::read_to_string(&statfile) {
+        Ok(data) => data,
+        Err(_) => return 0,
+    };
+    let data: Vec<&str> = data.split("(").collect();
+    if data.len() < 2 {
+        return 0;
+    }
+    let data = data[1];
+    let data: Vec<&str> = data.split(")").collect(); 
+    if data.len() < 1 {
+        return 0;
+    }
+    let data = data[0];
+    if data == EXE_NAME {
+        let pid: Vec<&str> = path.split("/").collect();
+        if pid.len() >= 3 {
+            let pid = pid[2];
+            return pid.parse().unwrap();
+        }
+    }
+    return 0;
 }
